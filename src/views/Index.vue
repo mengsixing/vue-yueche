883ecdae25df0edc69cf1c4dd2700de448cfe2da
Heekei <zxq0610@qq.com>
2018-11-04 14:51:08 +0800

    评价卡片组件

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -321,40 +321,45 @@
         </div>
       </main>
     </div>
+    <!-- 订单评价 -->
+    <evaluate v-if="showEvaluate"></evaluate>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import Evaluate from "./components/Evaluate.vue";
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: { Evaluate },
   props: {
     msg: String
   },
   data() {
     return {
+      showEvaluate:true,
       myInfo: {},
       serviceList: [],
-      hotLineList: [],
+      hotLineList: []
     };
   },
   created() {
     setTimeout(() => {
       this.initIndexPage();
-
     }, 1000);
     this.getMyInfo();
-    console.log('用户标识', '#(personId)');
+    console.log("用户标识", "#(personId)");
   },
   methods: {
     getMyInfo() {
-      axios.get('/getMyInfo?personId=#(personId)', (result) => {
+      axios.get("/getMyInfo?personId=#(personId)", result => {
         this.myInfo = result.data;
       });
-      axios.get('/getService', (result) => {
+      axios.get("/getService", result => {
         this.serviceList = result.data;
       });
-      axios.get('/getLinesList?isHot=1', (result) => {
+      axios.get("/getLinesList?isHot=1", result => {
         console.log(result);
         this.hotLineList = result.data;
       });
@@ -362,19 +367,20 @@ export default {
     initIndexPage() {
       //百度地图
       //创建地图并设置中心点和当前城市
-      var map = new BMap.Map('map', {
-        enableMapClick: false	//禁用地图默认的点击事件
+      var map = new BMap.Map("map", {
+        enableMapClick: false //禁用地图默认的点击事件
       });
-      var point = new BMap.Point(104.0818, 30.546564);	//成都某地
-      map.centerAndZoom(point, 7);	//中心点及级数
+      var point = new BMap.Point(104.0818, 30.546564); //成都某地
+      map.centerAndZoom(point, 7); //中心点及级数
       map.enableScrollWheelZoom(true);
-      map.setCurrentCity('成都');	//当前城市
+      map.setCurrentCity("成都"); //当前城市
       //标注：起点
       var point2 = new BMap.Point(104.0818, 30.546564);
-      var icon = new BMap.Icon('images/marker1x.png', new BMap.Size(27, 48), {	//标注大小
-        anchor: new BMap.Size(13.5, 43.5)	//标注的偏移量
+      var icon = new BMap.Icon("images/marker1x.png", new BMap.Size(27, 48), {
+        //标注大小
+        anchor: new BMap.Size(13.5, 43.5) //标注的偏移量
       });
-      var marker = new BMap.Marker(point2, { icon: icon });	//创建标注对象并添加到地图
+      var marker = new BMap.Marker(point2, { icon: icon }); //创建标注对象并添加到地图
       map.addOverlay(marker);
       //标注：终点
       /*var point2 = new BMap.Point(104.790778, 29.318791);	//自贡某地
@@ -386,33 +392,38 @@ export default {
       map.centerAndZoom(point2, 7);*/
 
       //切换线路TAB
-      $('.line nav a').click(function () {
-        $(this).addClass('active').siblings('a').removeClass('active');
+      $(".line nav a").click(function() {
+        $(this)
+          .addClass("active")
+          .siblings("a")
+          .removeClass("active");
         //显示：选择时间
-        if ($(this).text() == '预约') {
-          $('.line .select').addClass('show');
+        if ($(this).text() == "预约") {
+          $(".line .select").addClass("show");
         } else {
-          $('.line .select').removeClass('show');
+          $(".line .select").removeClass("show");
         }
       });
 
       //切换起点、终点
-      $('.line .main .switch').click(function () {
+      $(".line .main .switch").click(function() {
         //交换起点、终点的值
-        var t = $('.line .main li:first input').val();
-        $('.line .main li:first input').val($('.line .main li:last input').val());
-        $('.line .main li:last input').val(t);
+        var t = $(".line .main li:first input").val();
+        $(".line .main li:first input").val(
+          $(".line .main li:last input").val()
+        );
+        $(".line .main li:last input").val(t);
         //交换正向车费、逆向车费
-        var tt = $(this).attr('data-pay');
-        $(this).attr('data-pay', $(this).attr('data-pay2'));
-        $(this).attr('data-pay2', tt);
+        var tt = $(this).attr("data-pay");
+        $(this).attr("data-pay", $(this).attr("data-pay2"));
+        $(this).attr("data-pay2", tt);
         //显示当前车费
-        $('.line .price .pay span').text($(this).attr('data-pay'));
+        $(".line .price .pay span").text($(this).attr("data-pay"));
         //map.removeOverlay(marker);	//移除标识
       });
 
       //重新定位
-      $('.location div').click(function () {
+      $(".location div").click(function() {
         map.centerAndZoom(point, 7);
       });
 
@@ -424,63 +435,65 @@ export default {
       // });
 
       //预约时间框：有值状态
-      $(".line .select input").bind("input propertychange", function () {
+      $(".line .select input").bind("input propertychange", function() {
         var inputVal = $(this).val();
         if (inputVal) {
-          $('.line .select').addClass('active');
+          $(".line .select").addClass("active");
         }
       });
 
       //全部线路
       //打开面板
-      $('.line .main li input, .hotline .title a').click(function () {
-        $('#line').addClass('active');
+      $(".line .main li input, .hotline .title a").click(function() {
+        $("#line").addClass("active");
       });
       //关闭面板
-      $('#line').click(function (e) {
-        var _con = $('#line .box');
+      $("#line").click(function(e) {
+        var _con = $("#line .box");
         if (!_con.is(e.target) && _con.has(e.target).length === 0) {
-          $('#line').removeClass('active');
+          $("#line").removeClass("active");
         }
       });
       //点击列表项：关闭面板，并获取值
-      var start = '成都';	//起点：获取当前的城市
-      var end = '';	//终点
-      var price = 0;	//正向价格
-      var price2 = 0;	//逆向价格
-      var ticket = 0;	//优惠券
-      var pay = 0;	//正向车费
-      var pay2 = 0;	//逆向车费
-      $('#line li').click(function () {
-        $('#line').removeClass('active');
-        start = $(this).attr('data-start');
-        end = $(this).attr('data-end');
-        price = $(this).attr('data-price');
-        price2 = $(this).attr('data-price2');
-        ticket = $(this).attr('data-ticket');
+      var start = "成都"; //起点：获取当前的城市
+      var end = ""; //终点
+      var price = 0; //正向价格
+      var price2 = 0; //逆向价格
+      var ticket = 0; //优惠券
+      var pay = 0; //正向车费
+      var pay2 = 0; //逆向车费
+      $("#line li").click(function() {
+        $("#line").removeClass("active");
+        start = $(this).attr("data-start");
+        end = $(this).attr("data-end");
+        price = $(this).attr("data-price");
+        price2 = $(this).attr("data-price2");
+        ticket = $(this).attr("data-ticket");
         pay = parseFloat(price) - parseFloat(ticket);
         pay2 = parseFloat(price2) - parseFloat(ticket);
-        $('#startCity').val(start);	//起点城市
-        $('#endCity').val(end);	//终点城市
-        $('#pay').val(pay);	//应付正向车费
-        $('.line .price .pay span').text(pay);	//显示正向车费
-        if (ticket != 0) {	//有优惠券，显示优惠价格
-          $('.line .price .ticket').addClass('active');
-          $('.line .price .ticket span').text(ticket);
+        $("#startCity").val(start); //起点城市
+        $("#endCity").val(end); //终点城市
+        $("#pay").val(pay); //应付正向车费
+        $(".line .price .pay span").text(pay); //显示正向车费
+        if (ticket != 0) {
+          //有优惠券，显示优惠价格
+          $(".line .price .ticket").addClass("active");
+          $(".line .price .ticket span").text(ticket);
         } else {
-          $('.line .price .ticket').removeClass('active');
-          $('.line .price .ticket span').text(0);
+          $(".line .price .ticket").removeClass("active");
+          $(".line .price .ticket span").text(0);
         }
-        $('.line .price, main .button, main .hotline').addClass('active');	//显示：车费、提交按钮，不显示：热门线路
-        $('.line .main .switch').attr('data-pay', pay);	//正向车费
-        $('.line .main .switch').attr('data-pay2', pay2);	//逆向车费
+        $(".line .price, main .button, main .hotline").addClass("active"); //显示：车费、提交按钮，不显示：热门线路
+        $(".line .main .switch").attr("data-pay", pay); //正向车费
+        $(".line .main .switch").attr("data-pay2", pay2); //逆向车费
       });
     }
   }
-}
+};
 </script>
 
 <style scoped>
+@import url(../assets/css/waiting.css);
 /*
 for：业务首页
 date：2018-09
---
+++ src/views/components/Evaluate.vue
@@ -0,0 +1,194 @@
+<template>
+	<!--订单评价-->
+	<div class="active" id="evaluate" role="contentinfo">
+		<div class="evaluateBox">
+			<div class="evaluate">
+				<div class="uFace am-round am-center am-text-center">
+					<i class="am-icon-user"></i>
+					<img class="am-round" src="images/p2.jpg" alt="">
+				</div>
+				<div class="uInfo am-text-center">
+					<div>王师傅</div>
+					<span>川A29181G</span>
+				</div>
+				<div class="uStar am-text-center">
+					<i class="am-icon-star"></i>
+					<i class="am-icon-star"></i>
+					<i class="am-icon-star"></i>
+					<i class="am-icon-star"></i>
+					<i class="am-icon-star-half-o"></i>
+				</div>
+				<div id="eval-form" >
+					<div class="star am-center am-text-center">
+						<i class="am-icon-star"></i>
+						<i class="am-icon-star"></i>
+						<i class="am-icon-star"></i>
+						<i class="am-icon-star"></i>
+						<i class="am-icon-star"></i>
+						<input type="hidden" id="" name="" value="0">
+						<!--星级-->
+					</div>
+					<div class="fTip am-text-center">感谢您的评价，司机会更加努力</div>
+					<div class="checkbox am-center am-text-center">
+						<div class="am-round active">服务好<input type="checkbox" id="" name="" checked></div>
+						<div class="am-round">态度好<input type="checkbox" id="" name=""></div>
+						<div class="am-round">开车稳<input type="checkbox" id="" name=""></div>
+						<div class="am-round">车内整洁<input type="checkbox" id="" name=""></div>
+					</div>
+					<div class="fButton am-text-center"><button type="submit" class="am-btn am-btn-danger am-round">评价</button></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+/*
+for：订单评价
+date：2018-09
+*/
+/* CSS Document */
+#evaluate {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 21;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-transform: translateY(100%);
+  transform: translateY(100%);
+  -webkit-transition: -webkit-transform 0.3s;
+  transition: transform 0.3s;
+}
+#evaluate.active {
+  -webkit-transform: none;
+  transform: none;
+}
+.evaluateBox {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.evaluate {
+  position: absolute;
+  top: 50%;
+  left: 3.5%;
+  width: 93%;
+  transform: translateY(-50%);
+  background-color: #fff;
+  border-radius: 0.03rem;
+  display: table;
+}
+/*uFace*/
+.uFace {
+  width: 0.4rem;
+  height: 0.4rem;
+  margin-top: 0.22rem;
+  background-color: #f1f1f1;
+  font-size: 0;
+  position: relative;
+}
+.uFace i {
+  font-size: 0.25rem;
+  color: #ddd;
+  line-height: 0.4rem;
+}
+.uFace img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+}
+/*uInfo*/
+.uInfo {
+  margin-top: 0.05rem;
+  font-size: 0;
+}
+.uInfo div {
+  font-size: 0.12rem;
+  color: #757575;
+  line-height: 0.22rem;
+}
+.uInfo span {
+  font-size: 0.16rem;
+  color: #333;
+  line-height: 0.26rem;
+}
+/*uStar*/
+.uStar {
+  margin-top: 0.05rem;
+  font-size: 0;
+}
+.uStar i {
+  font-size: 0.23rem;
+  color: #ff9815;
+  margin: 0 0.03rem;
+}
+/*star*/
+.star {
+  width: 2.23rem;
+  margin-top: 0.14rem;
+  border-top: 1px solid #f1f1f1;
+  font-size: 0;
+}
+.star i {
+  font-size: 0.37rem;
+  color: #ccc;
+  margin: 0 0.04rem;
+  cursor: pointer;
+}
+.star i.active {
+  color: #ff9815;
+}
+/*fTip*/
+.fTip {
+  font-size: 0.14rem;
+  color: #757575;
+  line-height: 0.28rem;
+  padding-bottom: 0.03rem;
+}
+/*checkbox*/
+.checkbox {
+  width: 80%;
+  margin-top: 0.04rem;
+  font-size: 0;
+}
+.checkbox div {
+  font-size: 0.14rem;
+  color: #333;
+  line-height: 0.34rem;
+  border: 1px solid #ddd;
+  display: inline-block;
+  padding: 0 0.18rem;
+  -webkit-user-select: none;
+  user-select: none;
+  margin: 0.1rem 0.05rem 0;
+  cursor: pointer;
+}
+.checkbox div.active {
+  background-color: #5eb95e;
+  border-color: #5eb95e;
+  color: #fff;
+}
+.checkbox input {
+  display: none;
+}
+/*fButton*/
+.fButton {
+  margin-top: 0.19rem;
+  font-size: 0;
+  padding-bottom: 0.3rem;
+}
+</style>
