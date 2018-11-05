bc5ddfcb40c193dfcf8224a7c779eba301a48175
Heekei <zxq0610@qq.com>
2018-11-05 22:09:24 +0800

    封装公共头部组件

---
+++ src/views/components/ComHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <!--头部-->
+  <header id="header" role="heading">
+    <a class="back" href="javascript:history.go(-1);" title="">
+      <i class="am-icon-angle-left"></i>
+      <span>返回</span>
+    </a>
+    <h1 class="am-text-center">{{title}}</h1>
+  </header>
+</template>
+<script>
+export default {
+  props: ["title"],
+  methods: {}
+};
+</script>
+
+<style scoped>
+@import url(../../assets/css/header.css);
+</style>
