2a355af1d01b956dc2155a11e511cf822030a86e
Heekei <zxq0610@qq.com>
2018-11-11 19:45:34 +0800

    comPage样式调整

--- src/views/components/ComPage.vue
+++ src/views/components/ComPage.vue
@@ -18,8 +18,9 @@ export default {
 <style scoped>
 .container {
   height: calc(100vh - 0.52rem);
-  margin-top: .03rem;
+  margin-top: 0.03rem;
   overflow: auto;
   background-color: #f1f1f1;
+  padding-top: 0.1rem;
 }
 </style>
